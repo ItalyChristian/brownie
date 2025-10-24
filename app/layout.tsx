2cb768833649f53ae07d00469a2c463360a9b445
italy <italy.christian@hotmail.com>
2025-10-24 19:36:11 -0300

    updating title page icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Brownie",
   description: "Brownie",
+  icons: {
+    icon: "/brownie.png",
+  },
 };
 
 export default function RootLayout({
